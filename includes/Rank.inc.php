a91da7a145ed12686bc67fa8c298d1ded22a187a
Rui Oliveira <39498755+HackerinoS2@users.noreply.github.com>
2020-04-09 22:59:39 +0100

    Added rank names when getting the rank by player's name and added the "getPlayerRankById" method

--- includes/Rank.inc.php
+++ includes/Rank.inc.php
@@ -71,9 +71,51 @@ class Rank {
         $playerRank = $players[0]['ranked']['rank'];
         $playerMMR = $players[0]['ranked'][$playerRegion . '_mmr'];
 
+        //Gets a JSON file that translates the ranks numbers to its name | Example: 1 = Copper V, 2 = Copper IV...
+        $ranksJSON = file_get_contents('r6ranks.json', true);
+
+        //Decodes the json file to a variable (array)
+        $r6Ranks = json_decode($ranksJSON, true);
+
+        //Saves the real rank name
+        $playerRealRank = $r6Ranks[$playerRank];
+
+        //Sets up the r6tab profile URL of the player using is "user" (player id)
         $playerURL = 'https://r6tab.com/player/' . $playerUser;
 
         //Output results in plain text
-        echo "Region: $playerRegion | Rank: $playerRank | MMR: $playerMMR | Platform: $playerPlatform, more info in $playerURL"; //Setup output
+        echo "Region: $playerRegion | Rank: $playerRealRank | MMR: $playerMMR | Platform: $playerPlatform, more info in $playerURL";
+    }
+
+    function getPlayerRankById($id, $platform, $region) {
+        //Gets player data from r6Tab API
+        $playerDataRequest = file_get_contents("https://r6.apitab.com/player/$id");
+
+        //Outputs error when can't connect to r6Tab API
+        if (!$playerDataRequest) {
+            die("It was not possible to establish a connection with the r6Tab API");
+        }
+
+        //Decodes the received JSON data to a variable
+        $result = json_decode($playerDataRequest, true);
+
+        //If the request finds no results, output warning
+        if ($result['found'] == false) {
+            die("Couldn't find any player with the inserted ID");
+        }
+
+        //Gets the necessary data from the results
+        $playerName = $result['player']['p_name'];
+        $playerUser = $result['player']['p_user'];
+        $playerRegion = $region;
+        $playerPlatform = strtoupper($platform);
+        $playerRank = $result['ranked'][$playerRegion.'_rankname'];
+        $playerMMR = $result['ranked'][$playerRegion . '_mmr'];
+
+        //Sets up the r6tab profile URL of the player using is "user" (player id)
+        $playerURL = 'https://r6tab.com/player/' . $playerUser;
+
+        //Output results in plain text
+        echo "Region: $playerRegion | Rank: $playerRank | MMR: $playerMMR | Platform: $playerPlatform, more info in $playerURL";
     }
 }
